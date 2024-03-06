a1029ad574c4c0fc228dd8a66a45e6f76e587df8
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-03-06 15:30:28 +0100

    szamologep esemenykezeles

---
+++ fuggvenyek.js
@@ -0,0 +1,26 @@
+function gombok() {
+  let txt = "";
+  for (let i = 0; i < 10; i++) {
+    txt += `<button id = i${i}>${i}</button>`;
+  }
+  /* console.log(txt); */
+  return txt;
+}
+gombok();
+const SELECTION = document.getElementsByClassName("szamok");
+
+SELECTION[0].innerHTML = gombok();
+
+const SECTONGOMBOK = document.querySelectorAll(".szamok button");
+const KIFEJEZES = document.getElementsByClassName("kifejezes");
+
+for (let index = 0; index < SECTONGOMBOK.length; index++) {
+  SECTONGOMBOK[index].addEventListener("click", gombKattintas);
+}
+function gombKattintas(event) {
+console.log(event.target)
+/* event.target = kiírja azt az elemet, ami kiváltotta az eseményt */
+  for (let index = 0; index < 10; index++) {
+    KIFEJEZES[index].innerHTML += event.target.innerHTML;
+  }
+}
